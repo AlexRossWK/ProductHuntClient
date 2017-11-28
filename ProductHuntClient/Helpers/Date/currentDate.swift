ae40d15cc0995e44f808a46f4c60a6a73f2ecd4d
AlexRossWK <32623141+alexRossIOS@users.noreply.github.com>
2017-11-29 00:27:07 +0300

    data,cellAnimation

---
+++ ProductHuntClient/Helpers/Date/currentDate.swift
@@ -0,0 +1,9 @@
+//
+//  currentDate.swift
+//  ProductHuntClient
+//
+//  Created by Алексей Россошанский on 28.11.17.
+//  Copyright © 2017 Alexey Rossoshasky. All rights reserved.
+//
+
+import Foundation
---
+++ ProductHuntClient/Model/DateSingleton.swift
@@ -0,0 +1,9 @@
+//
+//  DateSingleton.swift
+//  ProductHuntClient
+//
+//  Created by Алексей Россошанский on 28.11.17.
+//  Copyright © 2017 Alexey Rossoshasky. All rights reserved.
+//
+
+import Foundation
